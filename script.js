9ba57a19ed1e3a126bccdddd09122e4ce0970a54
Vignes Kumar Raj M <vigneskumar@piktorlabs.com>
2022-08-23 18:23:56 +0530

    23/8/2022 6:23Pm

---
+++ script.js
@@ -0,0 +1,21 @@
+var cur = 'flex1';
+var temp = 'null';
+var init = 'g1'
+
+function clickCol(col) {
+    var a = col;
+    document.getElementById(cur).classList.remove("grow");
+    document.getElementById(a).classList.add("grow");
+    temp = a;
+    a = cur;
+    cur = temp;
+}
+
+function clickDiv(div) {
+    var b = div;
+    document.getElementById(init).classList.remove("grow");
+    document.getElementById(b).classList.add("grow");
+    temp = b;
+    b = init;
+    init = temp;
+}
